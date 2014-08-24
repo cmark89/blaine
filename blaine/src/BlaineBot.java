27220291363ce8a5662a851f41032ef6183d595c
cmark89 <cmark89@gmail.com>
2014-08-24 17:27:54 +0900

    Converted bot to use sikuli libraries

--- blaine/src/BlaineBot.java
+++ blaine/src/BlaineBot.java
@@ -5,20 +5,56 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Iterator;
+
+import org.sikuli.basics.Settings;
+import org.sikuli.script.*;
 
 public class BlaineBot {
 	BlaineWindow window;
 	ScreenScraper scraper;
 	Robot bot;
+	Thread thread;
+	Screen screen;
+	
 	boolean botRunning = false;
+	boolean playOnline = false;
+	
 	public boolean botIsRunning() {
 		return botRunning;
 	}
 	
+	private enum GameScreen {
+		NONE,
+		LOADING_SCREEN,
+		TITLE_SCREEN,
+		MODE_SELECT_SCREEN,
+		GAME_SCREEN
+	}
+	GameScreen currentGameScreen;
+	
+	// Variables for playing the game
+	Rectangle cardZone = new Rectangle();
+	Rectangle handZone = new Rectangle();
+	Point deckPosition = new Point();
+	Point ticketDeckPosition = new Point();
+	Point donePosition = new Point();
+	
+	final float DEFAULT_SCREEN_SEARCH_TIME = .3f;
+	
+	Rectangle wholeScreen;
+	Rectangle turnArea;
+	
 	public BlaineBot(BlaineWindow w) {
 		// Initialize logic here
 		window = w;
-		scraper = new ScreenScraper();
+		screen = new Screen();
+		screen.setAutoWaitTimeout(DEFAULT_SCREEN_SEARCH_TIME);
+		scraper = new ScreenScraper(screen);
+		wholeScreen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		turnArea = new Rectangle(wholeScreen.x, (wholeScreen.height / 3) * 2, 
+				wholeScreen.width, wholeScreen.height / 3);
 		try {
 			bot = new Robot();
 		} catch (AWTException e) {
@@ -29,8 +65,8 @@ public class BlaineBot {
 	
 	public void start() {
 		botRunning = true;
-		Thread t = new Thread(new BlaineJob_MainLoop());
-		t.start();
+		thread = new Thread(new BlaineJob_MainLoop());
+		thread.start();
 		System.out.println("All aboard!");
 	}
 	
@@ -39,12 +75,10 @@ public class BlaineBot {
 		System.out.println("Last stop!");
 	}
 	
-	private BufferedImage screen() {
+	private BufferedImage screenshot() {
 		return bot.createScreenCapture(new Rectangle(
 				Toolkit.getDefaultToolkit().getScreenSize()));
 	}
-	
-	
 	
 	class BlaineJob_MainLoop implements Runnable {
 
@@ -53,91 +87,190 @@ public class BlaineBot {
 						
 			// We must determine the initial state, but for now assume that we're on the main menu
 			window.forceMinimize();
-			int currentScreen = 0;
+			currentGameScreen = GameScreen.NONE;
+			
+			// Here's the main loop where we look at the current game screen and 
+			// decide what to do
+			while(botRunning) {
+				getCurrentGameScreen();
+				if(currentGameScreen != GameScreen.NONE) {
+					if(currentGameScreen == GameScreen.LOADING_SCREEN) {
+						System.out.println("Run load screen routine");
+						BotRoutine_LoadingScreen();
+					} else if(currentGameScreen == GameScreen.TITLE_SCREEN) {
+						System.out.println("Run main menu routine");
+						BotRoutine_MainMenu();
+					} else if(currentGameScreen == GameScreen.MODE_SELECT_SCREEN) {
+						System.out.println("Run mode select routine");
+						BotRoutine_ModeSelect();
+					} else if(currentGameScreen == GameScreen.GAME_SCREEN) {
+						System.out.println("Run game routine");
+						BotRoutine_PlayGame();
+					}
+				}
+				
+				sleep(250);
+			}
+		}
+		
+		// Routines to perform based on current game screen.  MUST RETURN TO THE MAIN LOOP
+		
+		private void BotRoutine_LoadingScreen() {
+			// We're on the loading screen, so the only thing to do is wait
+			sleep(1000);	// Wait one second and then return to the main loop
+			return;
+		}
+		
+		private void BotRoutine_MainMenu() {
+			// We're on the main menu, so we need to navigate to the game screen
+			Rectangle r = scraper.getBounds("mainmenu_key.png");
+			Point p = new Point(r.x + r.width / 2, r.y + r.height / 2);
+			System.out.println("Move mouse to " + p.x + "," + p.y);
+			bot.mouseMove(p.x, p.y);
+			sleep(25);
+			click();
+			
+			sleep(200);
+			return;
+		}
+		
+		private void BotRoutine_ModeSelect() {
+			// We're on the game mode select screen
+			// Time for some real work
 			Point p = new Point();
-			while(currentScreen == 0) {
-				sleep(1000);
-				if(scraper.findImage(screen(), "mainMenu_eyes", p)) {
-					// We found a point, but we now need to offset it by 185 to the right
-					p.x += 185;
-					System.out.println("MOVE MOUSE TO: " + p.x + "," + p.y);
+			Rectangle r = new Rectangle();
+			// Online button and offline button too similar; we need to use their pressed
+			// forms to make sure that they're differentiated properly
+			if(playOnline) {
+				Settings.MinSimilarity = .9f;
+				// We want to play online, so make sure the online option is selected
+				if(scraper.canFindImage("modeselect_onlineButton.png")) {
+					// We found the online button, so click it
+					r = scraper.getBounds("modeselect_onlineButton.png");
+					p = new Point(r.x + r.width / 2, r.y + r.height / 2);
 					bot.mouseMove(p.x, p.y);
-					sleep(50);
 					click();
-					
-					currentScreen++;
 				}
-			}
-			
-			while(currentScreen == 1) {
-				p = new Point(0,0);
-				// Try to find the select bar to make sure we're on the right mode
-				if(scraper.findImage(screen(), "modeSelect_bar", p)) {
-					p.x += 57;
+			} else {
+				Settings.MinSimilarity = .9f;
+				// We want to play offline, so make sure the offline option is selected
+				if(scraper.canFindImage("modeselect_soloButton.png")) {
+					// We found the online button, so click it
+					r = scraper.getBounds("modeselect_soloButton.png");
+					p = new Point(r.x + r.width / 2, r.y + r.height / 2);
 					bot.mouseMove(p.x, p.y);
-					sleep(50);
 					click();
-					
-					// Now start the game
-					scraper.findImage(screen(), "modeSelect_play", p);
-					p.y += 46;
-					bot.mouseMove(p.x, p.y);
-					sleep(50);
-					click();
-					
-					currentScreen++;
 				}
+				
+			}
+			sleep(250);
+			// Finally, click the play button
+			Settings.MinSimilarity = .7f;
+			r = scraper.getBounds("modeselect_startButton.png");
+			p = new Point(r.x + r.width / 2, r.y + r.height / 2);
+			bot.mouseMove(p.x, p.y);
+			click();
+			
+			sleep(1000);
+			return;
+		}
+		
+		private void BotRoutine_PlayGame() {
+			// This is going to be a monster...
+			
+			cardZone = new Rectangle();
+			handZone = new Rectangle();
+			deckPosition = new Point();
+			ticketDeckPosition = new Point();
+			donePosition = new Point();
+			
+			// First, have to wait until the game actually starts and the board appears
+			// We know the setup has begun when the grayed out Done button appears
+			boolean waiting = true;
+			while(!scraper.canFindImage("doneButton.png")) {
+				sleep(250);
 			}
 			
-			sleep(5000);
-			// We should now be in the game!
-			while(currentScreen == 2) {
-				// Start by taking all the cards, you greedy jerk
-				bot.mouseMove(570,655);
-				sleep(50);
+			// Okay, we've started the game.  Time to setup some values
+			Rectangle r = new Rectangle();
+			r = scraper.getBounds("doneButton.png");
+			donePosition = new Point(r.x + r.width / 2, r.y + r.height / 2);
+			r = scraper.getBounds("ticketDeck.png");
+			ticketDeckPosition = new Point(r.x + r.width / 2, r.y + r.height / 2);
+			r = scraper.getBounds("deck.png");
+			deckPosition = new Point(r.x + r.width / 2, r.y + r.height / 2);
+			
+			// Get the rectangle of the card area to speed up future searches
+			int x1, x2, y1, y2 = 0;
+			r = scraper.getBounds("cardzone_top.png");
+			x1 = r.x;
+			y1 = r.y;
+			r = scraper.getBounds("cardzone_bottom.png");
+			x2 = r.x + r.width;
+			y2 = r.y + r.height;
+			
+			cardZone = new Rectangle(x1,y1, x2 - x1, y2 - y1);
+			
+			// Print some diagnostic information here
+			System.out.println("Done button at: " + donePosition.x + "," + donePosition.y);
+			System.out.println("Ticket deck at: " + ticketDeckPosition.x + "," 
+					+ ticketDeckPosition.y);
+			System.out.println("Deck at: " + deckPosition.x + "," + deckPosition.y);
+			System.out.println("Card zone: " + cardZone.x + "," + cardZone.y + "," +
+					cardZone.width + "," + cardZone.height);
+			
+			// Okay, we now need to take all of our starting tickets for fun
+			Iterator<Match> tickets = scraper.findAll("ticket.png");
+			while(tickets.hasNext()) {
+				Match m = tickets.next();
+				// Find the ticket and click it
+				bot.mouseMove(m.getCenter().x, m.getCenter().y);
 				click();
-				
-				bot.mouseMove(724,655);
-				sleep(50);
-				click();
-				
-				bot.mouseMove(855,655);
-				sleep(50);
-				click();
-				
-				bot.mouseMove(1222, 672);
-				sleep(50);
-				click();
-				
-				currentScreen++;
+				sleep(100);
 			}
 			
-			// We are now in the main game loop
+			// click the done button
+			bot.mouseMove(donePosition.x, donePosition.y);
+			click();
+			
+			boolean gameOver = false;
+			while(!gameOver) {
+				// Wait until our turn
+				waitUntilTurn();
+				
+				System.out.println("It's our turn now!");
+				// It's our turn, so draw two cards
+				drawCard();
+				sleep(100);
+				drawCard();
+				sleep(100);
+			}
+		}
+		
+		private void waitUntilTurn() {
+			screen.setAutoWaitTimeout(.1f);
+			screen.setROI(turnArea);
 			boolean ourTurn = false;
-			while(true) {
-				sleep(200);
-				System.out.println("Waiting for turn...");
-				if(!ourTurn && scraper.findImage(screen(), "game_turnHint", 
-						new Rectangle(605, 573, 189, 41), p)) {
-					ourTurn = true;
-				}
-				
-				while(ourTurn) {
-					System.out.println("Blaine's turn!");
-					sleep(150);
-					// Just draw cards...
-					bot.mouseMove(1225, 465);
-					sleep(50);
-					click();
-					sleep(150);
-					click();
-					sleep(150);
-					
-					// Not our turn anymore
-					System.out.println("Turn over.");
-					ourTurn = false;
-				}
+			while(!ourTurn) {
+				sleep(100);
+				ourTurn = scraper.canFindImage("turnStart.png");
 			}
+			screen.setAutoWaitTimeout(DEFAULT_SCREEN_SEARCH_TIME);
+			screen.setROI(wholeScreen);
+			return;
+		}
+		
+		private void drawCard() {
+			System.out.println("MOVE MOUSE TO " + deckPosition.x +"," + deckPosition.y);
+			bot.mouseMove(deckPosition.x, deckPosition.y);
+			System.out.println("CLICK TO DRAW");
+			click();
+		}
+		
+		private void drawTickets() {
+			bot.mouseMove(deckPosition.x, deckPosition.y);
+			click();
+			sleep(250);
 		}
 		
 		private void sleep(int milli) {
@@ -154,6 +287,27 @@ public class BlaineBot {
 			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
 			sleep(50);
 			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+		}
+		
+		private void getCurrentGameScreen() {
+			
+			if (scraper.canFindImage("mainmenu_key.png")) {
+				currentGameScreen = GameScreen.TITLE_SCREEN;
+				System.out.println("ON MAIN MENU SCREEN");
+			} else if (scraper.canFindImage("modeselect_key.png")) {
+				currentGameScreen = GameScreen.MODE_SELECT_SCREEN;
+				System.out.println("ON MODE SELECT SCREEN");
+			} else if (scraper.canFindImage("gamescreen_key.png")) {
+				currentGameScreen = GameScreen.GAME_SCREEN;
+				System.out.println("ON GAME SCREEN");
+			} else if(scraper.canFindImage("loading_key.png")) {
+				currentGameScreen = GameScreen.LOADING_SCREEN;
+				System.out.println("ON LOADING SCREEN");
+			} else {
+				currentGameScreen = GameScreen.NONE;
+				System.out.println("GAME SCREEN NOT FOUND");
+			}
+			
 		}
 	}
 }
--- blaine/src/ScreenScraper.java
+++ blaine/src/ScreenScraper.java
@@ -6,103 +6,57 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.*;
 import java.io.File;
+import java.util.Iterator;
+
 import javax.imageio.ImageIO;
+
+import org.sikuli.script.*;
 
 public class ScreenScraper {
 	final String ASSET_PATH = "assets/";
-	public static void main(String[] args) {
-		ScreenScraper s = new ScreenScraper();
-	}
-	public ScreenScraper() {
-		test();
+	
+	Screen screen;
+	
+	public ScreenScraper(Screen s) {
+		screen = s;
 	}
 	
-	// Scrape the provided image for the image of the 
-	public void test() {
-		BufferedImage image = loadImage("mainMenu_eyes");
-		if(image == null)
-			System.out.println("Failed to load image");
-		else
-			System.out.println("Loaded image successfully");
-	}
-	
-	private BufferedImage loadImage(String path) {
-		/*
-		try {
-			return ImageIO.read(new File(ASSET_PATH + path +".png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-		*/
-		
-		BufferedImage raw;
-		try {
-			raw = ImageIO.read(new File(ASSET_PATH + path +".png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-		BufferedImage converted = new BufferedImage(raw.getWidth(), raw.getHeight(), 
-				BufferedImage.TYPE_INT_RGB);
-		converted.getGraphics().drawImage(raw, 0, 0, null);
-		converted.getGraphics().dispose();
-		return converted;
-		
-		// Load it using the type we agreed upon
-	}
-	
-	public boolean findImage(BufferedImage large, String smallName, Point p) {
-		return findImage(large, loadImage(smallName), p);
-	}
-	
-	public boolean findImage(BufferedImage large, String smallName, Rectangle searchArea, Point p) {
-		return findImage(large.getSubimage(searchArea.x, searchArea.y, 
-				searchArea.width, searchArea.height), loadImage(smallName), p);
-	}
-	
-	public boolean findImage(BufferedImage large, BufferedImage small, Point p) {
-		// Save a screenshot of what we're currently looking at		
-		
-		boolean found = false;
-		System.out.println("Target color: " + small.getRGB(0,0));
-		outerloop:
-		for(int x1 = 0; x1 < large.getWidth() - small.getWidth(); x1++) {
-			for(int y1 = 0; y1 < large.getHeight() - small.getHeight(); y1++) {
-				
-				innerloop:
-				for(int x2 = 0; x2 < small.getWidth(); x2++) {
-					for(int y2 = 0; y2 < small.getHeight(); y2++) {
-						if(large.getRGB(x1 + x2, y1 + y2) == small.getRGB(x2,y2))
-						{
-							System.out.println("Matched pixel at " + (x1 + x2) + "," 
-							+ (y1 + y2));
-
-							if(x2 == 0 && y2 == 0) p.setLocation(x1,y1);
-							if(x2 == small.getWidth() -1 && y2 == small.getHeight() - 1) {
-								// This is the final pixel and we have a match, so set found
-								// and break the loop
-								found = true;
-								break outerloop;
-							} else {
-								continue;
-							}
-						}
-						else {
-							break innerloop;
-						}
-					}
-				} // end inner loop
-			}
-		} // end outer loop
-		
-		// debug:
-		if(found) {
-			System.out.println("Found image at " + p.x + "," + p.y);
+	// Check if the screen contains the given image
+	public boolean canFindImage(String path) {
+		if(findImage(path) == null) {
+			return false;
 		} else {
-			System.out.println("Could not find image");
+			return true;
 		}
-		return found;
 	}
+	
+	public Match findImage(String path) {
+		Match m = null;
+		try {
+			m = screen.find(ASSET_PATH + path);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return m;
+	}
+	
+	public Iterator<Match> findAll(String path) {
+		Iterator<Match> m = null;
+		try {
+			m = screen.findAll(ASSET_PATH + path);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return m;
+	}
+	
+	public Rectangle getBounds(String path) {
+		Match m = findImage(path);
+		if(m != null) {
+			return new Rectangle(m.x, m.y, m.w, m.h);
+		} else {
+			return new Rectangle();
+		}
+	}
+	
 }
